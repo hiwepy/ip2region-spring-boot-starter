786bf57e4f9f35cf0d6c80bea5d0dee29ca019a5
wandl <hnxxyhcwdl1003@163.com>
2021-08-03 00:12:31 +0800

    优化

--- src/test/java/com/github/hiwepy/ip2region/spring/boot/Application.java
+++ src/test/java/com/github/hiwepy/ip2region/spring/boot/Application.java
@@ -23,7 +23,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableIP2region
 @SpringBootApplication
 public class Application {
 	
