7fb9d57b1c18ea334d22cc55fe775313d45b2a0e
wandl <hiwepy@163.com>
2023-08-22 02:23:32 +0800

    ip2region 集成实现

--- src/test/java/com/github/hiwepy/ip2region/spring/boot/Application.java
+++ src/test/java/com/github/hiwepy/ip2region/spring/boot/Application.java
@@ -34,10 +34,7 @@ public class Application {
 	@PostConstruct
 	public void test() throws IOException {
 
-	   System.out.println( template.btreeSearch("61.94.43.82"));
-	   System.out.println( template.binarySearch("61.94.43.82"));
 	   System.out.println( template.memorySearch("61.94.43.82"));
-	   System.out.println(template.binarySearch("127.0.0.1"));
 
 	   // 根据IP获取对应国家
 	   System.out.println(template.getCountryByIp("127.0.0.1"));
