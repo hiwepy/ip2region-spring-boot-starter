e100602e8d261f3d4e35a6aa965bd7073f1219df
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:05:56 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.0.x branch

---
+++ src/test/java/io/github/easy4j/ip2region/spring/boot/IP2RegionTemplate_Test.java
@@ -0,0 +1,54 @@
+package io.github.easy4j.ip2region.spring.boot;
+
+import io.github.easy4j.ip2region.spring.boot.ext.RegionAddress;
+import io.github.easy4j.ip2region.spring.boot.ext.RegionEnum;
+import io.github.easy4j.ip2region.spring.boot.ext.XdbSearcher;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.FileSystemResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+
+public class IP2RegionTemplate_Test {
+
+	IP2regionTemplate template = null;
+
+	@BeforeEach
+	public void setUp()  throws Exception {
+		String dbPath = "D:\\ip2region.xdb";
+		ResourceLoader resourceLoader = new FileSystemResourceLoader();
+		XdbSearcher xdbSearcher = new XdbSearcher(resourceLoader, dbPath);
+		template = new IP2regionTemplate(xdbSearcher);
+	}
+
+	@Test
+	public void templateTest() throws Exception {
+
+		System.out.println(template.memorySearch("127.0.0.1"));
+		System.out.println(template.memorySearch("114.124.146.103"));
+		System.out.println(template.memorySearch("115.193.169.84"));
+
+		System.out.println(template.getRegion("127.0.0.1"));
+		System.out.println(template.getRegion("114.124.146.103"));
+
+		RegionAddress adress1 = template.getRegionAddress("125.119.96.60");
+		System.out.println(adress1);
+		System.out.println(RegionEnum.getByRegionAddress(adress1));
+
+		RegionAddress adress2 = template.getRegionAddress("127.0.0.1");
+		System.out.println(adress2);
+		System.out.println(RegionEnum.getByRegionAddress(adress2));
+
+		RegionEnum regionEnum1 = template.getRegionByIp("127.0.0.1");
+		System.out.println(regionEnum1);
+		RegionEnum regionEnum2 = template.getRegionByIp("102.42.140.162");
+		System.out.println(regionEnum2);
+
+		System.out.println(template.getCountryByIp("127.0.0.1"));
+		System.out.println(template.getCountryByIp("114.124.146.103"));
+
+		System.out.println(template.isMainlandIp("127.0.0.1"));
+		System.out.println(template.isMainlandIp("114.124.146.103"));
+
+	}
+
+}
